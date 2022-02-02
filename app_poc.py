83f6af569ad8c7dbb70463736d0a9515a9187b4f
Damodar-9 <d.pulimamidi@gmail.com>
2022-02-02 15:01:37 +0530

    first commit

---
+++ app_poc.py
@@ -0,0 +1,191 @@
+import os
+import shutil
+from flask import Flask, redirect, url_for, request, flash
+from flask import render_template
+from flask import send_file
+
+# from flask import Flask, render_template, redirect, url_for
+# from flask_bootstrap import Bootstrap
+# from flask_wtf import FlaskForm
+# from wtforms import StringField, PasswordField, BooleanField
+# from wtforms.validators import InputRequired, Email, Length
+# from flask_sqlalchemy import SQLAlchemy
+# from werkzeug.security import generate_password_hash, check_password_hash
+# from flask_login import (
+#     LoginManager,
+#     UserMixin,
+#     login_user,
+#     login_required,
+#     logout_user,
+#     current_user,
+# )
+# from flask_admin import Admin
+# from flask_admin.contrib.sqla import ModelView
+
+app = Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+# app.config["SECRET_KEY"] = "Thisissupposedtobesecret!"
+# app.config[
+#     "SQLALCHEMY_DATABASE_URI"
+# ] = "sqlite:///C:\\Users\\damodar.pulimamidi\\OneDrive - aalgorithm.com\\Desktop\\my_poc\\mydatabase.db"
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        if "file" not in request.files:
+            flash("No files selected")
+            return redirect("/")
+        try:
+            shutil.rmtree("./images")
+        except:
+            pass
+        os.mkdir("./images")
+        files = request.files.getlist("file")
+        for f in files:
+            f.save(os.path.join("./images", f.filename))
+        for (dirpath, dirnames, filenames) in os.walk(app.config["IMAGES"]):
+            files = filenames
+            break
+        app.config["FILES"] = files
+        return redirect("/tagger", code=302)
+    else:
+        return render_template("index.html")
+
+
+@app.route("/tagger")
+def tagger():
+    if app.config["HEAD"] == len(app.config["FILES"]):
+        return redirect(url_for("final"))
+    directory = app.config["IMAGES"]
+    image = app.config["FILES"][app.config["HEAD"]]
+    labels = app.config["LABELS"]
+    not_end = not (app.config["HEAD"] == len(app.config["FILES"]) - 1)
+    print(not_end)
+    return render_template(
+        "tagger.html",
+        not_end=not_end,
+        directory=directory,
+        image=image,
+        labels=labels,
+        head=app.config["HEAD"] + 1,
+        len=len(app.config["FILES"]),
+    )
+
+
+@app.route("/next")
+def next():
+    image = app.config["FILES"][app.config["HEAD"]]
+    app.config["HEAD"] = app.config["HEAD"] + 1
+    with open(app.config["OUT"], "a") as f:
+        for label in app.config["LABELS"]:
+            f.write(
+                image
+                + ","
+                + label["id"]
+                + ","
+                + label["name"]
+                + ","
+                + str(round(float(label["xMin"])))
+                + ","
+                + str(round(float(label["xMax"])))
+                + ","
+                + str(round(float(label["yMin"])))
+                + ","
+                + str(round(float(label["yMax"])))
+                + "\n"
+            )
+    app.config["LABELS"] = []
+    return redirect(url_for("tagger"))
+
+
+#############
+@app.route("/previous")
+def prev():
+    image = app.config["FILES"][app.config["HEAD"]]
+    app.config["HEAD"] = app.config["HEAD"] - 1
+    with open(app.config["OUT"], "a") as f:
+        for label in app.config["LABELS"]:
+            f.write(
+                image
+                + ","
+                + label["id"]
+                + ","
+                + label["name"]
+                + ","
+                + str(round(float(label["xMin"])))
+                + ","
+                + str(round(float(label["xMax"])))
+                + ","
+                + str(round(float(label["yMin"])))
+                + ","
+                + str(round(float(label["yMax"])))
+                + "\n"
+            )
+    app.config["LABELS"] = []
+    return redirect(url_for("tagger"))
+
+
+########
+@app.route("/final")
+def final():
+    return render_template("final.html")
+
+
+@app.route("/add/<id>")
+def add(id):
+    xMin = request.args.get("xMin")
+    xMax = request.args.get("xMax")
+    yMin = request.args.get("yMin")
+    yMax = request.args.get("yMax")
+    app.config["LABELS"].append(
+        {"id": id, "name": "", "xMin": xMin, "xMax": xMax, "yMin": yMin, "yMax": yMax}
+    )
+    return redirect(url_for("tagger"))
+
+
+@app.route("/remove/<id>")
+def remove(id):
+    index = int(id) - 1
+    del app.config["LABELS"][index]
+    for label in app.config["LABELS"][index:]:
+        label["id"] = str(int(label["id"]) - 1)
+    return redirect(url_for("tagger"))
+
+
+@app.route("/label/<id>")
+def label(id):
+    name = request.args.get("name")
+    app.config["LABELS"][int(id) - 1]["name"] = name
+    return redirect(url_for("tagger"))
+
+
+@app.route("/image/<f>")
+def images(f):
+    images = app.config["IMAGES"]
+    return send_file(images + "/" + f)
+
+
+@app.route("/download")
+def download():
+    shutil.copyfile("out.csv", "images/annotations.csv")
+    shutil.make_archive("final", "zip", "images")
+    return send_file(
+        "final.zip",
+        mimetype="text/csv",
+        attachment_filename="final.zip",
+        as_attachment=True,
+    )
+
+
+if __name__ == "__main__":
+    app.config["IMAGES"] = "images"
+    app.config["LABELS"] = []
+    app.config["HEAD"] = 0
+    app.config["OUT"] = "out.csv"
+    with open("out.csv", "w") as f:
+        f.write("image,id,name,xMin,xMax,yMin,yMax\n")
+    app.run(debug="True")
+
+    # app.run(host="0.0.0.0", port=5001)
